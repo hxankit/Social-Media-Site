1414b4cfd44fadf5d8fb2fb4e1590546a96864d1
Ankit <hxankit01@gmail.com>
2025-04-10 19:09:02 +0530

    New test

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import getDataUri from "../utils/DataUri.js";
+import getDataUri from "../utils/datauri.js";
 import cloudinary from "../utils/cloudinary.js";
 import { Post } from "../models/post.model.js";
 import { Otp } from "../models/Otp.js";
--- Backend/utils/dataUri.js
+++ Backend/utils/dataUri.js
@@ -3,8 +3,8 @@ import path from "path";
 
 const parser = new DataUriParser();
 
-const GetDataUri = (file) => {
+const getDataUri = (file) => {
     const extName = path.extname(file.originalname).toString();
     return parser.format(extName, file.buffer).content;
 };
-export default GetDataUri;
+export default getDataUri;
