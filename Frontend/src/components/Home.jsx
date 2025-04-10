b520dfa383ffc239d8a69d03e783586851102c22
Ankit <hxankit01@gmail.com>
2025-04-10 10:23:39 +0530

    production issue

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Feed from './Feed';
 import { Outlet } from 'react-router-dom';
-import RightSidebar from './RightSidebar';
+import RightSidebar from './RightSidebar.jsx';
 import useGetAllPost from '@/hooks/useGetAllPost';
 import useGetSuggestedUsers from '@/hooks/useGetSuggestedUsers';
 
